814b3820b36a0a3c3ad6cc2901570eaa796effab
João Pedro Patrício do Nascimento <joaoppn00@gmail.com>
2024-04-15 21:46:42 -0300

    Eu e as Camisas

--- slide.js
+++ slide.js
@@ -1,8 +1,13 @@
 var images = [
     'images/tshit1.png',
     'images/tshit2.png',
-    'images/tshit5.png',
     'images/tshit4.png',
+    'images/tshit3.png',
+    'images/tshit5.png',
+    'images/tshit6.png',
+    'images/tshit7.png',
+    'images/tshit10.png',
+    'images/tshit8.png',
   ];
 
   var num = 0;
