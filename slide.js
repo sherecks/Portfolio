a42c24ce2b963215f5c4c9181f5f8a8543a3054b
João Pedro Patrício do Nascimento <joaoppn00@gmail.com>
2024-04-25 20:05:54 -0300

    Ar

--- slide.js
+++ slide.js
@@ -8,16 +8,30 @@ var images = [
     'images/cargo.png',
     'images/fleece2.png',
     'images/tshit2.png',
+    'images/cargo3.png',
+    'images/fleece3.png',
     'images/tshit1.png',
   ];
 
   var num = 0;
+  var image;
 
-  function btn(){
-  var image = document.getElementById("image");
-  num++;
-  if ( num >= images.length ) {
-    num = 0;
+  document.addEventListener('DOMContentLoaded', function() {
+    image = document.getElementById("image");
+  });
+
+  function nextImage() {
+    num++;
+    if (num >= images.length) {
+      num = 0;
+    }
+    image.src = images[num];
   }
-  image.src = images[num];
-}
+  
+  function prevImage() {
+    num--;
+    if (num < 0) {
+      num = images.length - 1;
+    }
+    image.src = images[num];
+  }
