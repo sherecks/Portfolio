65239424460efede9439bec824af1881e3a00a11
João Pedro Patrício do Nascimento <joaoppn00@gmail.com>
2024-09-03 18:29:09 -0300

    6.0

--- preloadImagens.js
+++ preloadImagens.js
@@ -1,17 +1,9 @@
 function preloadImages() {
     var images = [
-        'images/berma.png',
-        'images/berma4.png',
-        'images/cargo2.png',
-        'images/berma2.png',
-        'images/cargo.png',
         'images/tshit2.png',
-        'images/cargo3.png',
-        'images/berma3.png',
         'images/tshit1.png',
         'images/tshit4.png',
-        'images/prev.png',
-        'images/next.png',
+        
 
     ];
 
--- slide.js
+++ slide.js
@@ -1,16 +1,7 @@
 var images = [
-    'images/berma.png',
-    'images/cargo2.png',
-    'images/berma4.png',
-    'images/berma2.png',
-    'images/tshit1.png',
-    'images/cargo.png',
     'images/tshit2.png',
-    'images/cargo3.png',
-    'images/tshit5.png',
-    'images/tshit6.png',
-    'images/berma3.png',
     'images/tshit4.png',
+    'images/tshit1.png',
   ];
 
   var num = 0;
