56bbeded4ce63ff4f5df519016b43ce4a9997f57
João Pedro Patrício do Nascimento <joaoppn00@gmail.com>
2024-10-09 14:45:43 -0300

    Correções

--- preloadImagens.js
+++ preloadImagens.js
@@ -1,21 +1,15 @@
 function preloadImages() {
     var images = [
-        'images/2.png',
-        'images/3.png',
-        'images/4.png',
-        'images/1.png',
-        'images/BR1.png',
+        'images/4K_7_1.png',
+        'images/4K_7_2.png',
+        'images/4K_7_4.png',
+        'images/Kimarco-1.png',
+        'images/BR4.png',
         'images/BR3.png',
-        'images/BR2.png',
-        'images/tshit1.png',
-        'images/tshit2.png',
-        'images/tshit5.png',
-        'images/tshit4.png',
         'images/l1.png',
         'images/l2.png',
-        'images/h4.png',
-        'images/h3.png',
-        
+        'images/TshitBlue2.png',
+        'images/TshitBlue3.png',
     ];
 
     images.forEach(function(image) {
